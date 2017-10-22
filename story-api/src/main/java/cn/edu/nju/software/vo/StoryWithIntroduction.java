d8f2688246f0a4d5266d718021a3985d759ceac5
zs <806949096@qq.com>
2017-10-22 12:42:39 +0800

    add storyintroduction api
    Signed-off-by: zs <806949096@qq.com>

---
+++ story-api/src/main/java/cn/edu/nju/software/vo/StoryWithIntroduction.java
@@ -0,0 +1,19 @@
+package cn.edu.nju.software.vo;
+
+import cn.edu.nju.software.entity.Story;
+
+/**
+ * @author zs1996 E-mail:806949096@qq.com
+ * @version 创建时间：2017年10月22日
+ */
+public class StoryWithIntroduction extends Story {
+	private String introduction;
+
+	public String getIntroduction() {
+		return introduction;
+	}
+
+	public void setIntroduction(String introduction) {
+		this.introduction = introduction;
+	}
+}
