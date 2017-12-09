e01ea8a3ce42c78837de54264393aae47cc9d951
xmc1993 <xumaocheng@baidu.com>
2017-12-09 14:15:00 +0800

    f

--- service-api/src/main/java/cn/edu/nju/software/service/impl/OssServiceImpl.java
+++ service-api/src/main/java/cn/edu/nju/software/service/impl/OssServiceImpl.java
@@ -29,7 +29,8 @@ import java.util.Map;
 public class OssServiceImpl implements OssService {
     private static Logger logger = LoggerFactory.getLogger(OssServiceImpl.class);
     private final static String host = "http://warmtale-backend-bucket.oss-cn-beijing.aliyuncs.com";
-    private final static String accessId = "LTAIQLXhrN5gIJlk";
+    private final static String accessId = "LTAI9RtjsTqUlw10";
+//    private final static String accessId = "LTAIQLXhrN5gIJlk";
 
     @Autowired
     private OSSClient client;
